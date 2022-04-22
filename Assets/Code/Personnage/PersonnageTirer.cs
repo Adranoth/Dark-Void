9fdd2b160d5d01e8fc61f44d631fef5298dc4b13
Adranoth <arcaman2000@gmail.com>
2022-04-22 14:13:19 -0400

    main

--- Assets/Code/Personnage/PersonnageTirer.cs
+++ Assets/Code/Personnage/PersonnageTirer.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class PersonnageTirer : MonoBehaviour
 {
     public Inventaire inventaire;
+    private PersonnageMouvements personnageMouvements;
 
     public Transform sortieDeLaBalle;
     public GameObject ballePrefab;
@@ -15,6 +16,16 @@ public class PersonnageTirer : MonoBehaviour
 
     public float rechargementDuree = 3f;
     private bool recharge;
+
+    private bool attenteProchaineBalle = false;
+    public float tempsMaxEntreBalles = 0.5f;
+    public float tempsMinEntreBalles = 0.1f;
+    public float tempsPleineVitesse = 3f;
+    private float acceleration = 0f;
+    private float tempsDepuisTir = 0f;
+
+    public float angleHasardeuxMax = 5f;
+    private float angleHasardeuxActuel = 0f;
 
     AudioSource audiosource;
     public AudioClip tirer;
@@ -26,18 +37,31 @@ public class PersonnageTirer : MonoBehaviour
         CompteurMun.text = inventaire.munitionsActuelle.ToString();
         recharge = false;
         audiosource = GetComponent<AudioSource>();
+        personnageMouvements = gameObject.GetComponent<PersonnageMouvements>();
     }
 
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButton("Fire1"))
         {
+            tempsDepuisTir = 0;
             Tirer();
         }
+        else
+        {
+            acceleration -= tempsDepuisTir / tempsPleineVitesse;
+            tempsDepuisTir += Time.deltaTime;
+            if(acceleration < 0)
+            {
+                acceleration = 0;
+            }
 
-        if(Input.GetKeyDown(KeyCode.R))
+        }
+
+        if(Input.GetKeyDown(KeyCode.R) && !recharge && inventaire.chargeursActuels > 0)
         {
             Recharger();
+            acceleration = 0;
         }
     }
 
@@ -54,6 +78,17 @@ public class PersonnageTirer : MonoBehaviour
         recharge = false;
     }
 
+    public IEnumerator IntervalleEntreLesTirs()
+    {
+        attenteProchaineBalle = true;
+        if(acceleration > 1)
+        {
+            acceleration = 1;
+        }
+        yield return new WaitForSeconds(Mathf.Lerp(tempsMaxEntreBalles, tempsMinEntreBalles, acceleration));
+        attenteProchaineBalle = false;
+    }
+
     void Recharger()
     {
         if (inventaire.chargeursActuels == 0 || (inventaire.munitionsActuelle == inventaire.munitionsMax))
@@ -68,20 +103,36 @@ public class PersonnageTirer : MonoBehaviour
 
     void Tirer()
     {
-        if (inventaire.munitionsActuelle == 0 || recharge)
+        acceleration += Time.deltaTime / tempsPleineVitesse;
+
+        angleHasardeuxActuel = Mathf.Lerp(0f, angleHasardeuxMax, acceleration);
+        angleHasardeuxActuel = Random.Range(-angleHasardeuxActuel, angleHasardeuxActuel) * Mathf.Deg2Rad;
+        Quaternion rotationHasardeuse = sortieDeLaBalle.rotation;
+
+        if (personnageMouvements.faceADroite)
+        {
+            rotationHasardeuse.z = rotationHasardeuse.z + angleHasardeuxActuel;
+        }else
+        {
+            rotationHasardeuse.x = rotationHasardeuse.x + angleHasardeuxActuel;
+        }
+
+        if ((inventaire.munitionsActuelle == 0 || recharge) && !attenteProchaineBalle)
         {
             audiosource.PlayOneShot(plusDeMunitions, 2f);
+            StartCoroutine(IntervalleEntreLesTirs());
         }
-        else
+        else if (!attenteProchaineBalle)
         {
             audiosource.PlayOneShot(tirer, 1.3f);
-            Instantiate(ballePrefab, sortieDeLaBalle.position, sortieDeLaBalle.rotation);
+            Instantiate(ballePrefab, sortieDeLaBalle.position, rotationHasardeuse);
             inventaire.munitionsActuelle -= 1;
             CompteurMun.text = inventaire.munitionsActuelle.ToString();
             if (inventaire.munitionsActuelle <= 15)
             {
                 CompteurMun.color = new Color(1f, 0f, 0f, 1f);
             }
+            StartCoroutine(IntervalleEntreLesTirs());
         }
     }
 }
